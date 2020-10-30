0211fcc9e9ed61fadc8b588079e02945d8e3f1fe
Gumani Ratshisuka <62070830+Gumie22@users.noreply.github.com>
2020-10-30 15:56:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿//This is a C# 4 operators basic calculator program
+using System;
+
+namespace Calculator.cs
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a number: "); //Prompts user to enter the first number
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Enter Operator: ");//Prompts user to enter the operator they want to use
+            string op = Console.ReadLine();
+            
+            Console.Write("Enter a number: ");//Prompts user to enter the second number
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+//Calculates and prints out the result
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator"); //If user enters an operator not mentioned above
+            }
+        }
+    }
+}
